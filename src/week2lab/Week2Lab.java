2d9a47bc9c74ac55226922b03c2edf9c993773c1
User <User@DESKTOP-U2C5NI7>
2022-09-15 08:52:56 -0600

    New Project Creation Created a new Java Application for Week02Lab.

---
+++ src/week2lab/Week2Lab.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week2lab;
+
+/**
+ *
+ * @author User
+ */
+public class Week2Lab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Sorry I have database assignment to do so cant be creative now");
+    }
+    
+}
